5af027057409efe9ba2a9356eaf9f81193674ec5
Van Toan <vantoan777e1@gmail.com>
2026-03-14 23:35:39 +0700

    java_errorcmt

---
+++ jvjava_error.java
@@ -0,0 +1,14 @@
+public class StudentScore {
+
+    public double calculateAverage(int score1, int score2, int score3){
+        int total = score1 + score2 + score3;
+        double average = total / 3.0; 
+        return average;
+    }
+
+    public static void main(String[] args){
+        StudentScore student = new StudentScore();
+        double avg = student.calculateAverage(8, 7, 9);
+        System.out.println("Average score: " + avg);
+    }
+}
